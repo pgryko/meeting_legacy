5faf0817a4d5b220e053ea9a7923993370ed9e20
Piotr Gryko <piotr.gryko@gmail.com>
2016-04-04 06:51:48 +0100

    Added possible solution to being unable to pull url - need to test if this works with both https and http

--- src/lib/components/meeting-grid-view-item.jsx
+++ src/lib/components/meeting-grid-view-item.jsx
@@ -118,7 +118,8 @@ export default class MeetingGridViewItem extends React.Component {
                                 height: frameHeight + 'px',
                             }}
                             scrolling="no"
-                            src={this.props.url}
+                            {/*Added https to prevent localhost from being added, need to test this */}
+                            src={"https://" + this.props.url}
                             onLoad={() => this.setState({loading: false})} />
 
                         <MeetingProgressView
